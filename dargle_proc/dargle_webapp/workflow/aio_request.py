9e610105df9a5f33d6a91f7e063b4eeb5f2cf0a5
JL <JL>
2021-06-17 02:22:34 -0400

    general cleaning of code

--- dargle_proc/dargle_webapp/workflow/aio_request.py
+++ dargle_proc/dargle_webapp/workflow/aio_request.py
@@ -14,7 +14,6 @@ async def get_page(url, hits, session, sem):
     # make sure the url has http:// on it
     if (url[0:4] != 'http'):
         url = 'http://' + url
-    
     # clean up any whitespace or newlines
     url = url.rstrip('\n')
 
@@ -36,11 +35,14 @@ async def get_page(url, hits, session, sem):
             timeout = 60
         try:
             start_req = time.time()
+
+            # the actual request
             async with sem, session.get(url, timeout=timeout) as r:
 
                 #dictating the output encoding helps tremendusly with preformance
                 text = await r.text(encoding='utf-8')
 
+                #load everything you want into dict
                 ret['request_time'] = (time.time() - start_req)
                 ret['title'] = (await parse_for_title(text))[2: -1]
                 ret['status'] = r.status
@@ -106,23 +108,40 @@ async def parse_for_title(text):
 
         return title
     except Exception as e:
-        print("b4 err: " + str(e))
+        err_msg = "+++++++++ b4 err: " + str(e)
+        print(err_msg)
+
+        return err_msg
 
 
-# the main working method.  it mainly just triggers the reading head and sets up stuff
+
+def write_out(results, outie):
+    # write the results to file
+    try:
+        writer = csv.writer(open(outie, 'w+'))
+        for ret in results:
+            if ret is not None:
+                if 'error' in ret:
+                    writer.writerow([ret['url'],ret['error'],ret['hits'],ret['timestamp'],"N/A"])
+                else:
+                    writer.writerow([ret['url'],ret['status'],ret['hits'],ret['timestamp'],ret['title']])
+            else:
+                writer.writerow(["NONE"])
+    except Exception as e:
+        print("Error in writing outfile: " + str(e))
+
+
+# the main working fucntion
 async def main(innie,outie,header):
 
-    #TODO: remove debugline
-    print("start of main")
-
-    # setup the csv file into dict format
+    # setup the csv file to be read from
     infile = open(innie, 'r')
     in_reader = csv.reader(infile, delimiter=',')
     if header == 'true':
         next(in_reader,None)
 
     # max number of sessions open
-    sem = asyncio.Semaphore(1000)
+    sem = asyncio.Semaphore(400)
 
     #http headers
     headers = {
@@ -132,41 +151,25 @@ async def main(innie,outie,header):
     #proxy
     proxy = "socks5://localhost:9050"
     connector = ProxyConnector.from_url(proxy)
+
+
     #set up the session
     async with aiohttp.ClientSession(headers=headers, connector=connector) as session:
         # this is the complicated part.  makes a buncha "sessions" named tasks and then collects them at the end.
-        # this triggers the reading head method at the top.  row[0] = url, row[1] = hits
         tasks = []
         for row in in_reader:
             task = asyncio.create_task(get_page(row[0], row[1], session, sem))
             tasks.append(task)
 
-
-        # I dont know why this works, but it does.  Helps control the None that get returned.
-        # I assume that this is caused by the sessions being gathered before they returned.
-        await asyncio.sleep(3) 
-
         # gather up the "sessions" and wait for them to all end
         results = await asyncio.gather(*tasks)
 
-
-        try:
-            writer = csv.writer(open(outie, 'w+'))
-            for ret in results:
-                if ret is not None:
-                    if 'error' in ret:
-                        writer.writerow([ret['url'],ret['error'],ret['hits'],ret['timestamp'],"N/A"])
-                    else:
-                        writer.writerow([ret['url'],ret['status'],ret['hits'],ret['timestamp'],ret['title']])
-                else:
-                    writer.writerow(["NONE"])
-        except Exception as e:
-            print("Error in writing outfile: " + str(e))
+        #write the results to file
+        write_out(results, outie)
 
 
 
 # this is triggered by external proccess and kicks off the main async def
-# innie, outie, header
 def proccess_links(innie,outie,header):
 
     # count the lines in the file
@@ -174,14 +177,14 @@ def proccess_links(innie,outie,header):
         for i, l in enumerate(f):
             pass
     f.close()
-    print(i+1)
+    print("Number of sites loade: " + str(i+1))
 
     #start timer
     start_time = time.time()
 
     # windows bug workaround 'https://stackoverflow.com/a/66772242'
-    
     #asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
     # this sets up and takes down the working loop and stuff.  also handles closing "sessions" cause that is complicated apparently
     asyncio.run(main(innie,outie,header))
 
