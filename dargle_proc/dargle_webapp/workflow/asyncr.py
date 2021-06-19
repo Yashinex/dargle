6b71c49a70d22fe8172b6fecf1a3cf65d2abb687
JL <JL>
2021-06-19 18:22:53 -0400

    filled out functions

--- dargle_proc/dargle_webapp/workflow/asyncr.py
+++ dargle_proc/dargle_webapp/workflow/asyncr.py
@@ -2,16 +2,36 @@ import aiohttp
 import asyncio
 import csv
 import time
-import sys
 
-import traceback
 
 from bs4 import BeautifulSoup
 from datetime import datetime
 from aiohttp_socks import ProxyConnector as pc
-import python_socks
 
-def process_links(innie,outie,header):
+
+async def runner(url, hits, session):
+	# set up dict to contain page info
+	response = {}
+	response['url'] = url.strip()
+	response['hits'] = hits
+
+	# get the page
+	async with session.get(url, timeout=20) as r:
+		text = await r.content.read(-1)
+		status = r.status
+
+	# parse title
+	soup = BeautifulSoup(text, 'lxml')
+	response['title'] = soup.title.string
+
+	response['status'] = status
+
+	response['timestamp'] = (datetime.now()).strftime("%m/%d/%Y %H:%M:%S")
+
+	return response
+
+
+async def main(innie,outie,header):
 	infile = open(innie, 'r')
 	outfile = open(outie, 'w+')
 
@@ -28,4 +48,32 @@ def process_links(innie,outie,header):
 
 	# SOCKS Proxy
 	proxy = "socks5://localhost:9050"
-	connect = pc.from_url(proxy)
+	connector = pc.from_url(proxy)
+	
+	# Spin off runners to get pages
+	async with aiohttp.ClientSession(connector=connector) as session:
+		tasks = []
+		for url in in_reader:
+			task = asyncio.create_task(runner(url[0], url[1], session))
+			tasks.append(task)
+		
+		# gather the runners when they return
+		results = await asyncio.gather(*tasks)
+
+		# put the results the outfile
+		for result in results:
+			out_writer.writerow([result['url'], result['status'], result['hits'], results['timestamps'], results['title']])
+
+
+# this method triggers main
+def proccess_links(innie,outie,header):
+	# timer for testing
+	start_time = time.time()
+
+	# patches selector loop selection bug
+	asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+	# start the async with debug enabled
+	asyncio.run(main(innie,outie,header), debug=True)
+
+	# end the timer
+	print("--- %s seconds ---" % (time.time() - start_time))
